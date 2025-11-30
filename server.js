018ed7cefb4b7dbaa650b318346d378cc73c2711
20KT1A0567 <venkataraodama660@gmail.com>
2025-11-30 06:39:58 +0530

    venkat

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const attendanceRoutes = require('./routes/attendance');
 const dashboardRoutes = require('./routes/dashboard');
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -18,9 +18,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/attendance', attendanceRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use(cors({
-  origin: "https://employee-manager18.netlify.app/",
+  origin: "https://employee-manager18.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
